c50931c09b0bac85b046e965a1811f82ac111b4b
Brian Buchalter <bal711@gmail.com>
2017-08-18 05:17:53 -0400

    Use BooksAPI to render books with correct content
    Since each Book knows it's ID, we can use the API to get the information
    needed to render the Book with the correct content.
    
    Although this may seem like an inefficent way to do this, as we've
    already queried `BooksAPI.getAll`, HTTP caching makes this operation
    relatively inexpensive in the long term. It would be nice to see if you
    could add a local caching layer, but we'll see if there's time for that
    later.

--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import * as BooksAPI from './BooksAPI';
 
 class Book extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coverImageUrl: '',
+      title: '',
+      authors: ''
+    }
+  }
+
+  componentDidMount() {
+    BooksAPI.get(this.props.id).then((book) => {
+      this.setState({
+        coverImageUrl: book.imageLinks.smallThumbnail,
+        title: book.title,
+        authors: book.authors
+      })
+    });
+  }
+
   render() {
     return(
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+          <div className="book-cover" style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url("${this.state.coverImageUrl}")`
+          }}></div>
           <div className="book-shelf-changer">
             <select>
               <option value="none" disabled>Move to...</option>
@@ -16,9 +40,8 @@ class Book extends React.Component {
             </select>
           </div>
         </div>
-        <div className="book-title">To Kill a Mockingbird</div>
-        <div className="book-authors">Harper Lee</div>
-        <div>{this.props.id}</div>
+        <div className="book-title">{this.state.title}</div>
+        <div className="book-authors">{this.state.authors}</div>
       </div>
     );
   }

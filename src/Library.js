16b76a8fa1d8657c4ff0624962f38fb2bf335ab1
Brian Buchalter <bal711@gmail.com>
2017-08-18 05:02:53 -0400

    Construct library state and render bookshelves
    Using the API we can define a Libary's state such that:
    * We know the names of all shelves
    * We know which book is on each shelf
    
    This state is the same structure which will be returned by
    BooksAPI.update, making it easy to update the Library's state once the
    shelf changer is implemented.

--- src/Library.js
+++ src/Library.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI'
 import Bookshelf from './Bookshelf';
 
 class Library extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shelves: {},
+      shelfNames: [],
+    };
+  }
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      const shelves = {}
+      const shelfNames = []
+
+      books.forEach(book => {
+        if(shelves[book.shelf] !== undefined) {
+          shelves[book.shelf].push(book.id)
+        } else {
+          shelfNames.push(book.shelf)
+          shelves[book.shelf] = [book.id]
+        }
+      });
+      this.setState({
+        shelves: shelves,
+        shelfNames: shelfNames,
+      });
+    });
+  }
+
   render() {
     return(
       <div className="list-books">
@@ -11,19 +39,13 @@ class Library extends React.Component {
         </div>
         <div className="list-books-content">
           <div>
-            {this.renderShelves()}
+            {this.state.shelfNames.map(name => <Bookshelf key={name} name={name} />)}
           </div>
         </div>
         <div className="open-search">
           <Link to="/search">Add a book</Link>
         </div>
       </div>
-    );
-  }
-
-  renderShelves() {
-    return(
-      <Bookshelf name="test" />
     );
   }
 }

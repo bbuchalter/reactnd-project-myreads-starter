740e6ceab326b397af1512cd4548c16c7556b1cb
Brian Buchalter <bal711@gmail.com>
2017-08-18 04:19:23 -0400

    Use react-router
    A lot less code to manage routing now!

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
 import './App.css'
 import SearchPage from './SearchPage';
 import Library from './Library';
 
 class BooksApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.togglePage = this.togglePage.bind(this);
-  }
-
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true
-  }
-
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchPage togglePage={this.togglePage} />
-        ) : (
-          <Library togglePage={this.togglePage} />
-        )}
-      </div>
+      <Router>
+        <div className="app">
+          <Route exact path="/" component={Library}/>
+          <Route exact path="/search" component={SearchPage}/>
+        </div>
+      </Router>
     )
-  }
-
-  togglePage() {
-    console.log('click!')
-    this.setState((prevState) => (
-      { showSearchPage: !prevState.showSearchPage }
-    ));
   }
 }
 
